9ab213e44e12f0c115fa90272a816cd0d7c7460e
Smyja <akposlive59@gmail.com>
2022-01-05 22:27:52 +0100

    completed design

--- scrimba-react/imba/src/components/Navbar.js
+++ scrimba-react/imba/src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { ReactComponent as Logo } from '../images/logo.svg';
+import '../index.css'
 
 function Navbar() {
     return (
-        <h1>Navbar</h1>
+        <nav><Logo style={{width:"100px",height:"45px"}}/><h3>About</h3><h2>Contact</h2></nav>
             
     
     )
